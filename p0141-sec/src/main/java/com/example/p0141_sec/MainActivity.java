1b492b12951451f809ad3810883fde8e3a6c0261
Anellla <anellla.anellla@gmail.com>
2018-04-19 02:28:04 +0300

    not work

--- p0141-sec/src/main/java/com/example/p0141_sec/MainActivity.java
+++ p0141-sec/src/main/java/com/example/p0141_sec/MainActivity.java
@@ -4,11 +4,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.CheckBox;
 
 public class MainActivity extends AppCompatActivity {
     CheckBox grfr, grcherry, grvish;
+    MenuItem fruits, pear, plum, tomato, cherry, vishnya;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,17 +19,34 @@ public class MainActivity extends AppCompatActivity {
         grcherry = (CheckBox) findViewById(R.id.grcherry);
         grvish = (CheckBox) findViewById(R.id.grvish);
 
+        fruits = (MenuItem) findViewById(R.id.fruits);
+        cherry = (MenuItem) findViewById(R.id.cherry);
+
+
+
         View.OnClickListener onClickListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 switch (v.getId()) {
-                    case (R.id.grfr):
-
-
+                    case R.id.grfr:
+                        if (grfr.isChecked())
+                            fruits.setVisible(true);
+                        else
+                            fruits.setVisible(false);
+                    break;
+                    case R.id.grcherry:
+                        if (grcherry.isChecked())
+                            cherry.setVisible(true);
+                        else
+                            cherry.setVisible(false);
+                    break;
                 }
 
             }
-        }
+        };
+        grfr.setOnClickListener(onClickListener);
+        grcherry.setOnClickListener(onClickListener);
+
     }
 
     @Override
@@ -36,4 +55,6 @@ public class MainActivity extends AppCompatActivity {
         inflater.inflate(R.menu.menu, menu);
         return true;
     }
+
+
 }
